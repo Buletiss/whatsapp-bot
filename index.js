591becd4ccf882da54bb088373daf4840b508fc7
Buletiss <vinicius1997feitosa@gmail.com>
2023-03-29 08:49:22 -0300

    feat: :sparkles: save local auth

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const client = new Client({
+  authStrategy: new LocalAuth()
+})
+
 
 client.on('qr', qr => {
-  qrcode.generate(qr, { small: true });
+  qrcode.generate(qr, { small: true }, function(qr) {
+    console.log(qr)
+  });
 });
+
+
 
 client.on('ready', () => {
   console.log('Client Ready');
 });
 
-const text = 'banana';
+// async function sendMediaUrl() {
+//   const media = await MessageMedia.fromUrl("https://picsum.photos/200/300")
+//   return media
+// }
+
+// const media = await MessageMedia.fromUrl("https://picsum.photos/200/300")
+
+const text = 'Teste';
 
 client.on('message', message => {
   console.log(message.body);
-  if (message.body === 'teste') {
+  if (message.body == message.body) {
     console.log('wpp msg: ', message.from);
     client.sendMessage(message.from, text);
   }
