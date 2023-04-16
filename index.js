1d66d0b3f1779dc97b9281439b50771fe6a0bcae
Buletiss <vinicius1997feitosa@gmail.com>
2023-04-16 04:37:02 -0300

    feat: :sparkles: config AI_GPT

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const qrcode = require('qrcode-terminal');
 
+
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const client = new Client({
   authStrategy: new LocalAuth()
 })
 
+const {Configuration, OpenAIApi, Configuration} = require("openai")
+const configuration = new Configuration({
+  organization: "org-tkrMgoLwSh23QIQkNuCL1Uvd",
+  apiKey: process.env.API_KEY,
+}) 
+
+const openai = new OpenAIApi(configuration)
+const response = await open.listEngines()
 
 client.on('qr', qr => {
   qrcode.generate(qr, { small: true }, function(qr) {
