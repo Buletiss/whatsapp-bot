7302d3a697d9633bbe0de0e51a1d2ce5ae2a55e5
Buletiss <vinicius1997feitosa@gmail.com>
2023-04-17 21:51:37 -0300

    chore: :wrench: update webjs lib e add .env no gitignore

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-const express = require('express')
-const app = express()
-const {Router} = require("express")
-const routes = Router()
+const express = require('express');
+const app = express();
+const { Router } = require('express');
+const routes = Router();
 const qrcode = require('qrcode-terminal');
+require('dotenv').config();
 
 //epxress config
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333, (error) => {
-  if(error) {
-    console.log("port error")
+app.listen(3333, error => {
+  if (error) {
+    console.log('port error');
   }
-})
+});
 
 //wpp web confi
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const client = new Client({
-  authStrategy: new LocalAuth()
-})
-
-//openAi config
-const {Configuration, OpenAIApi, Configuration} = require("openai")
-const configuration = new Configuration({
-  organization: "org-tkrMgoLwSh23QIQkNuCL1Uvd",
-  apiKey: process.env.API_KEY,
-}) 
-
-const openai = new OpenAIApi(configuration)
-const response = await open.listEngines()
+  authStrategy: new LocalAuth(),
+});
 
 //gera um qr code para sincronizar com o wpp
 client.on('qr', qr => {
-  qrcode.generate(qr, { small: true }, function(qr) {
-    console.log(qr)
+  qrcode.generate(qr, { small: true }, function (qr) {
+    console.log(qr);
   });
 });
 
@@ -43,9 +34,18 @@ client.on('ready', () => {
 });
 
 //teste de msg para o proprio wpp
-client.on('message_create', async (msg_create) => {
-  console.log(msg_create.body)
-})
-
+client.on('message_create', async msg_create => {
+  console.log(msg_create.body);
+});
 
 client.initialize();
+
+//openAi config
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  organization: 'org-tkrMgoLwSh23QIQkNuCL1Uvd',
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const response = openai.listEngines();
